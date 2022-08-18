5db1ebe2b01532bd71f3f3d25bac20b3c155860f
PriyadarsiniNandakumar <pdn@laufire.in>
2022-08-18 16:31:55 +0530

    Added  Component  Logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import { React } from 'react';
+import Logo from './components/logo';
 import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="logo">
-			<div className="container">
-				<div className="box b1"> </div>
-				<div className="box b2"> </div>
-				<div className="box b3"> </div>
-				<div className="box b4"> </div>
-			</div>
-		</div>
+		<Logo/>
 	</div>
 	;
 
