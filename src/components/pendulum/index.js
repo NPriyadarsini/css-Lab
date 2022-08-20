e54ca1a3d0c8a3d6c7c265cdc1594cdd76e13966
PriyadarsiniNandakumar <pdn@laufire.in>
2022-08-20 18:47:23 +0530

    added animation for pendulum

--- src/components/pendulum/index.js
+++ src/components/pendulum/index.js
@@ -2,8 +2,9 @@ import { React } from 'react';
 import './index.css';
 
 const Pendulum = () =>
-	<div className="pendulum">
-		<div className="container">
+	<div className="container">
+		<div className="square"> </div>
+		<div className="pendulum">
 			<div className="line"> </div>
 			<div className="circle"> </div>
 		</div>
