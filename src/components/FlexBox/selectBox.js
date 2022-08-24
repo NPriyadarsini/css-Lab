5efee84020c036a6b40eeb4e8dd89f545e3eb691
PriyadarsiniNandakumar <pdn@laufire.in>
2022-08-24 19:46:27 +0530

    Improved Flex Box

--- src/components/FlexBox/selectBox.js
+++ src/components/FlexBox/selectBox.js
@@ -4,7 +4,8 @@ import context from '../../core/context';
 const SelectBox = () => {
 	const { actions, state, config: { flexColor }} = context;
 
-	return <div>
+	return <div id="flex-select">
+		Select Box Color:{}
 		<select
 			value={ state.selectedFlex }
 			onChange={ (evt) => actions.setFlexColor(evt.target.value) }
